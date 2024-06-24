c15fbedc10f3ac64f27be31e62228797b4c5b8c3
andrearuocco <ruoca96@gmail.com>
2024-06-24 18:55:23 +0200

    artist

--- script/artist.js
+++ script/artist.js
@@ -10,6 +10,33 @@ const getDataAr = async () => {
     return data;
 };
 
+const newFetch = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
+
 getDataAr().then(data => {
-    console.log(data)
+    let imageAr = data.picture_big
+    const IMG = document.querySelector(".jumbotron img")
+    IMG.src = data.picture_big
+    const H1 = document.querySelector("#name")
+    H1.innerText = data.name
+    const FAN = document.getElementById("fan")
+    FAN.innerText = data.nb_fan + " ascoltatori mensili"
+    newFetch(data.tracklist).then(data => {
+        //console.log(data)
+        const FIVE = document.getElementById("five")
+        const ONE = document.getElementById("one")
+        data.data.slice(0, 5).forEach(element => {
+            console.log(element)
+            FIVE.innerHTML += `<ul class='mx-1 d-flex list-unstyled align-items-center text-white'><li class='p-2'><i class='fa-solid fa-play d-none'></i></li><li class='d-flex col-6'><img src='${element.album.cover_small}' class='cinqpx px50 me-2'/><h6>${element.title}</h6></li><li class='col-2'>${element.rank}</li><li class='col-1'></li><li class='d-flex col-2 align-items-center'><i class='fa-regular fa-heart d-none p-2'></i><span class='ms-2'>${element.duration}</span><i class='fa-solid fa-ellipsis p-2 d-none'></i></li></ul>`
+        });
+        data.data.slice(1, 2).forEach(element => {
+            console.log(element.contributors.picture_small)
+            ONE.innerHTML += `<img src='${element.album.cover_small}' class='br-20 centpx px100 col-6 me-2'/><div class='col-5'>
+            <div class='d-flex bg-cb align-items-center br-20'><img src='${imageAr}' class='br-50 col-6 ms-2 pi-px px-pi py-0'/><p class='py-2'>FUORI ORA!</p></div>
+            <h5 class='text-white'>${element.title}</h5></div>`
+        })
+    })
 })
--- script/script.js
+++ script/script.js
@@ -34,7 +34,7 @@ const listArtist = async () => {
         <div class="card bg-transparent col-12 col-sm-6 col-md-3 col-lg-2 position-relative">
             <img src="${element.artist.picture_medium}" class="card-img-top br-50">
             <div class="card-body">
-              <small><h6 class="text-white card-title">${element.artist.name}</h6></small>
+              <small><h6 id="h6" class="text-white card-title">${element.artist.name}</h6></small>
               <p class="card-text text-white text-opacity-50">Artist</p>
             </div>
             <a href="artist.html?id=${element.artist.id}" class="btn btn-success position-absolute br-pa d-none"><i class='fa-solid fa-play p-2'></i></a> 
@@ -48,7 +48,7 @@ const listArtist = async () => {
           <div class="card bg-transparent col-12 col-sm-6 col-md-3 col-lg-2 position-relative">
               <img src="${album.cover_medium}" class="card-img-top br-50">
               <div class="card-body">
-                <small><h6 class="text-white card-title">${album.title}</h6></small>
+                <small><h6 id="h6" class="text-white card-title">${album.title}</h6></small>
                 <small><small><p class="card-text text-white text-opacity-50">${album.artist.name}</p></small></small>
               </div>
               <a href="album.html?id=${album.id}" class="btn btn-success position-absolute br-pa d-none"><i class='fa-solid fa-play p-2'></i></a> 
