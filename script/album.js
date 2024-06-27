16662d31a346a3d49f4fb57749933d7220f59a77
andrearuocco <ruoca96@gmail.com>
2024-06-27 12:29:12 +0200

    fuerteventura

--- script/album.js
+++ script/album.js
@@ -10,6 +10,17 @@ const getDataAl = async () => {
     return data;
 };
 
+function play(attributes) {
+    console.log(attributes)
+    const AUDIO = document.querySelector("source")
+    const PLAY = document.getElementById("audio")
+    AUDIO.src = `${attributes}`
+    PLAY.load()
+    const ICON = document.querySelector("#none i")
+    ICON.classList.add("d-none")
+    PLAY.classList.remove("d-none")
+}
+
 let dataName = ""
 getDataAl().then(data => {
     //console.log(data.tracks.data)
@@ -27,7 +38,7 @@ getDataAl().then(data => {
 
     const ALBUM = document.querySelector(".album")
     for(index of data.tracks.data) {
-        console.log(index)
+        //console.log(index.preview)
         ALBUM.innerHTML += `
         <div id='tracks' class='d-flex justify-content-between me-4 mb-2'>
             <ul class='d-flex list-unstyled align-items-center text-white'>
@@ -35,55 +46,13 @@ getDataAl().then(data => {
                 <li class='d-flex flex-column'><h6>${index.title}</h6><p>${index.artist.name}</p></li>
             </ul>
             <ul class='d-flex list-unstyled align-items-center text-white'>
-                <li class='pe-4'><i class='fa-regular fa-heart d-none p-2'></i></li>
+                <li class='pe-4'><i onclick="play('${index.preview}')" id="cuore" class='fa-regular fa-heart d-none p-2'></i></li>
                 <li class=''>${index.duration}</li>
                 <li class=''><i class='fa-solid fa-ellipsis p-2 d-none'></i></li>
             </ul> 
         </div>`
     }
 })
-
-/*
-function searchIcon() {
-    
-    let input = "https://striveschool-api.herokuapp.com/api/deezer/search?q="
-    const FORM = document.querySelector(".form-control").value.toLowerCase()
-    input += FORM
-    fetch(input).then(response => {
-        response.json().then(data => {
-            console.log(data)
-        })
-    })
-
-
-    let input = "https://striveschool-api.herokuapp.com/api/deezer/search?q="
-    const FORM = document.querySelector(".form-control").value.toLowerCase()
-    input += FORM
-    fetch(input).then(response => {
-        response.json().then(data => {
-            console.log(data)
-            /* 
-            const ARTIST = document.getElementById("artist")
-            const ALBUM = document.getElementById("album")
-            ARTIST.innerHTML = "<h1 class='col-12 text-white pt-3'>Popular Albums</h1>"
-            ALBUM.classList.add("d-none")
-            data.data.slice(0, 6).forEach((element) => {
-                //console.log(element.artist)
-                ARTIST.innerHTML += `
-            <div class="card bg-transparent col-12 col-sm-6 col-md-3 col-lg-2 position-relative">
-              <img src="${element.album.cover_medium}" class="card-img-top br-50">
-              <div class="card-body">
-                <small><h6 id="h6" class="text-white card-title">${element.title}</h6></small>
-                <p class="card-text text-white text-opacity-50">${element.artist.name}</p>
-              </div>
-              <a href="artist.html?id=${element.artist.id}" class="btn btn-success position-absolute br-pa d-none"><i class='fa-solid fa-play p-2'></i></a> 
-            </div>`
-            })
-            
-        })
-    })
-} 
-*/
 
 document.addEventListener('scroll', function() {
     const TOP = document.querySelector(".top-bar ul:first-child")
